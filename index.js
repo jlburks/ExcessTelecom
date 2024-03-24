7f9dbe177a82d465943d4f36c401088d7826a49a
jlbur <jlburks1500@gmail.com>
2024-03-24 14:26:48 -0500

    nav display upgraded

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("mySidenav").style.width = "20%";
+  document.getElementById("mainContent").style.marginLeft = "25%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mainContent").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
+}
+
+function toHomePage() {
+  document.getElementById("homePage").style.display = "block";
+  document.getElementById("aboutPage").style.display = "none";
+  document.getElementById("devicesPage").style.display = "none";
+  document.getElementById("internetPage").style.display = "none";
+  closeNav();
+}
+
+function toInternetPage() {
+  document.getElementById("internetPage").style.display = "block";
+  document.getElementById("aboutPage").style.display = "none";
+  document.getElementById("devicesPage").style.display = "none";
+  document.getElementById("homePage").style.display = "none";
+  closeNav();
+}
+
+function toAboutPage() {
+  document.getElementById("aboutPage").style.display = "block";
+  document.getElementById("internetPage").style.display = "none";
+  document.getElementById("devicesPage").style.display = "none";
+  document.getElementById("homePage").style.display = "none";
+  closeNav();
+}
+function toDevicesPage() {
+  document.getElementById("devicesPage").style.display = "block";
+  document.getElementById("internetPage").style.display = "none";
+  document.getElementById("aboutPage").style.display = "none";
+  document.getElementById("homePage").style.display = "none";
+  closeNav();
 }
